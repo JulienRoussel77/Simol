73ea11e0349d833dec572e8e314cc4d155296d6c
Cedric Doucet <cedric.doucet@inria.fr>
2015-03-02 09:50:25 +0100

    Add an example of yaml-cpp usage

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,28 @@
 #include "SparseMatrix.hpp"
 #include <Eigen/Eigenvalues>
 
+#include <yaml-cpp/yaml.h>
+
 int main()
 {
+
+  //==========
+  // YAML
+  //==========
+  
+  YAML::Node node;  // starts out as null
+  node["key"] = "value";  // it now is a map node
+  node["seq"].push_back("first element");  // node["seq"] automatically becomes a sequence
+  node["seq"].push_back("second element");
+
+  node["mirror"] = node["seq"][0];  // this creates an alias
+  node["seq"][0] = "1st element";  // this also changes node["mirror"]
+  node["mirror"] = "element #1";  // and this changes node["seq"][0] - they're really the "same" node
+
+  node["self"] = node;  // you can even create self-aliases
+  node[node["mirror"]] = node["seq"];  // and strange loops :)
+
+  std::cout << YAML::Dump(node) << std::endl;
 
   //===============
   // MATRIX LOADING
