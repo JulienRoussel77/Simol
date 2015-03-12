eec3d50b9cf2f5029b1f6ae62a08136b2ebe8ec9
Cedric Doucet <cedric.doucet@inria.fr>
2015-03-12 13:42:24 +0100

    Create arpack.hpp including all interfaces to arparck's subroutines

--- src/core/dsaupd.hpp
+++ src/core/dsaupd.hpp
@@ -1,32 +1,35 @@
-#ifndef DSAUPD_HPP
-#define DSAUPD_HPP
+#ifndef SIMOL_DSAUPD_HPP
+#define SIMOL_DSAUPD_HPP
 
-extern "C"
+namespace simol
 {
-  void dsaupd_(int *ido, char *bmat, int *n, char *which,
-               int *nev, double *tol, double *resid,
-               int *ncv, double *v, int *ldv,
-               int *iparam, int *ipntr, double *workd,
-               double *workl, int *lworkl, int *info);
+
+  extern "C"
+  {
+    void dsaupd_(int *ido, char *bmat, int *n, char *which,
+                 int *nev, double *tol, double *resid,
+                 int *ncv, double *v, int *ldv,
+                 int *iparam, int *ipntr, double *workd,
+                 double *workl, int *lworkl, int *info);
+  }
+
+  void dsaupd(int &ido, char &bmat, int &n, char *which,
+              int &nev, double &tol, double *resid,
+              int &ncv, double *v, int &ldv,
+              int *iparam, int *ipntr, double *workd,
+              double *workl, int &lworkl, int &info);
+
+  inline
+  void dsaupd(int &ido, char &bmat, int &n, char *which,
+              int &nev, double &tol, double *resid,
+              int &ncv, double *v, int &ldv,
+              int *iparam, int *ipntr, double *workd,
+              double *workl, int &lworkl, int &info)
+  {
+    dsaupd_(&ido, &bmat, &n, which, &nev, &tol, resid, 
+            &ncv, v, &ldv, iparam, ipntr, workd, workl, 
+           &lworkl, &info);
+  }
 }
-
-void dsaupd(int &ido, char &bmat, int &n, char *which,
-            int &nev, double &tol, double *resid,
-            int &ncv, double *v, int &ldv,
-            int *iparam, int *ipntr, double *workd,
-            double *workl, int &lworkl, int &info);
-
-inline
-void dsaupd(int &ido, char &bmat, int &n, char *which,
-            int &nev, double &tol, double *resid,
-            int &ncv, double *v, int &ldv,
-            int *iparam, int *ipntr, double *workd,
-            double *workl, int &lworkl, int &info)
-{
-  dsaupd_(&ido, &bmat, &n, which, &nev, &tol, resid, 
-          &ncv, v, &ldv, iparam, ipntr, workd, workl, 
-          &lworkl, &info);
-}
-
 
 #endif
--- src/core/dseupd.hpp
+++ src/core/dseupd.hpp
@@ -1,41 +1,46 @@
-#ifndef DSEUPD_HPP
-#define DSEUPD_HPP
+#ifndef SIMOL_DSEUPD_HPP
+#define SIMOL_DSEUPD_HPP
 
-extern "C"
+namespace simol
 {
-  void dseupd_(bool *rvec, char *HowMny, bool *select,
-               double *d, double *Z, int *ldz,
-               double *sigma, char *bmat, int *n,
-               char *which, int *nev, double *tol,
-               double *resid, int *ncv, double *V,
-               int *ldv, int *iparam, int *ipntr,
-               double *workd, double *workl,
-               int *lworkl, int *info);
-}
 
-void dseupd(bool &rvec, char &HowMny, bool *select,
-            double *d, double *Z, int &ldz,
-            double &sigma, char &bmat, int &n,
-            char *which, int &nev, double &tol,
-            double *resid, int &ncv, double *V,
-            int &ldv, int *iparam, int *ipntr,
-            double *workd, double *workl,
-            int &lworkl, int &info);
+  extern "C"
+  {
+    void dseupd_(bool *rvec, char *HowMny, bool *select,
+                 double *d, double *Z, int *ldz,
+                 double *sigma, char *bmat, int *n,
+                 char *which, int *nev, double *tol,
+                 double *resid, int *ncv, double *V,
+                 int *ldv, int *iparam, int *ipntr,
+                 double *workd, double *workl,
+                 int *lworkl, int *info);
+  }
 
-inline
-void dseupd(bool &rvec, char &howmny, bool *select,
-            double *d, double *z, int &ldz,
-            double &sigma, char &bmat, int &n,
-            char *which, int &nev, double &tol,
-            double *resid, int &ncv, double *v,
-            int &ldv, int *iparam, int *ipntr,
-            double *workd, double *workl,
-            int &lworkl, int &info)
-{
-  dseupd_( &rvec, &howmny, select, d, z, &ldz, &sigma, 
-           &bmat, &n, which, &nev, &tol, resid, &ncv, 
-           v, &ldv, iparam, ipntr, workd, workl, &lworkl, 
-           &info );
+  void dseupd(bool &rvec, char &HowMny, bool *select,
+              double *d, double *Z, int &ldz,
+              double &sigma, char &bmat, int &n,
+              char *which, int &nev, double &tol,
+              double *resid, int &ncv, double *V,
+              int &ldv, int *iparam, int *ipntr,
+              double *workd, double *workl,
+              int &lworkl, int &info);
+
+  inline
+  void dseupd(bool &rvec, char &howmny, bool *select,
+              double *d, double *z, int &ldz,
+              double &sigma, char &bmat, int &n,
+              char *which, int &nev, double &tol,
+              double *resid, int &ncv, double *v,
+              int &ldv, int *iparam, int *ipntr,
+             double *workd, double *workl,
+              int &lworkl, int &info)
+  {
+    dseupd_( &rvec, &howmny, select, d, z, &ldz, &sigma, 
+             &bmat, &n, which, &nev, &tol, resid, &ncv, 
+             v, &ldv, iparam, ipntr, workd, workl, &lworkl, 
+             &info );
+  }
+
 }
 
 #endif
