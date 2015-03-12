339a94e3b127aa9e35ba9314bcc75a88e837e100
Cedric Doucet <cedric.doucet@inria.fr>
2015-03-12 13:20:14 +0100

    Add C++ interfaces to Arpack's subroutines dsaupd and dseupd

---
+++ src/core/dsaupd.hpp
@@ -0,0 +1,32 @@
+#ifndef DSAUPD_HPP
+#define DSAUPD_HPP
+
+extern "C"
+{
+  void dsaupd_(int *ido, char *bmat, int *n, char *which,
+               int *nev, double *tol, double *resid,
+               int *ncv, double *v, int *ldv,
+               int *iparam, int *ipntr, double *workd,
+               double *workl, int *lworkl, int *info);
+}
+
+void dsaupd(int &ido, char &bmat, int &n, char *which,
+            int &nev, double &tol, double *resid,
+            int &ncv, double *v, int &ldv,
+            int *iparam, int *ipntr, double *workd,
+            double *workl, int &lworkl, int &info);
+
+inline
+void dsaupd(int &ido, char &bmat, int &n, char *which,
+            int &nev, double &tol, double *resid,
+            int &ncv, double *v, int &ldv,
+            int *iparam, int *ipntr, double *workd,
+            double *workl, int &lworkl, int &info)
+{
+  dsaupd_(&ido, &bmat, &n, which, &nev, &tol, resid, 
+          &ncv, v, &ldv, iparam, ipntr, workd, workl, 
+          &lworkl, &info);
+}
+
+
+#endif
