f3481e3a2d5c5a00bd32b5b845f29bfc6f06b76d
Cedric Doucet <cedric.doucet@inria.fr>
2015-05-06 14:00:24 +0200

    Remove a spurious file inclusion

--- src/core/statphys/HamiltonianDynamics.hpp
+++ src/core/statphys/HamiltonianDynamics.hpp
@@ -1,14 +1,12 @@
 #ifndef SIMOL_HAMILTONIANDYNAMICS_HPP
 #define SIMOL_HAMILTONIANDYNAMICS_HPP
 
-#include "MolecularDynamics.hpp"
 
 namespace simol
 {
 
   template<class ScalarType>
   class HamiltonianDynamics
-  : public MolecularDynamics<ScalarType>
   {
     public:
       HamiltonianDynamics(ScalarType mass,
