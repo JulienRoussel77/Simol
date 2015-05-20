0d6d80f131e7a6522f60d8b5f8513fc50c7665e7
Cedric Doucet <cedric.doucet@inria.fr>
2015-05-20 21:56:39 +0200

    Fix a bug: forgot to set _INSTALL_DIR in parent scope at the end of DownloadThenInstallFunction

--- cmake/modules/DownloadThenInstall.cmake
+++ cmake/modules/DownloadThenInstall.cmake
@@ -1,18 +1,26 @@
 function(DownloadThenInstall LIBRARY_NAMES)
 
-  foreach(LIBRARY_NAME IN LISTS LIBRARY_NAMES)
+foreach(LIBRARY_NAME IN LISTS LIBRARY_NAMES)
 
     string(TOLOWER ${LIBRARY_NAME} LIBRARY_LOWERNAME)
     string(TOUPPER ${LIBRARY_NAME} LIBRARY_UPPERNAME)
 
+    set(${LIBRARY_UPPERNAME}_PREFIX ${EXTERNAL_DIR}/${LIBRARY_LOWERNAME} )
+    set(${LIBRARY_UPPERNAME}_DOWNLOAD_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/download)
+    set(${LIBRARY_UPPERNAME}_SOURCE_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/src)
+    set(${LIBRARY_UPPERNAME}_BINARY_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/build)
+    set(${LIBRARY_UPPERNAME}_TMP_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/tmp)
+    set(${LIBRARY_UPPERNAME}_STAMP_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/stamp)
+    set(${LIBRARY_UPPERNAME}_INSTALL_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/install)
+
     ExternalProject_Add(${LIBRARY_LOWERNAME}
-                        PREFIX ${EXTERNAL_DIR}/${LIBRARY_LOWERNAME}
-                        DOWNLOAD_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/download
-                        SOURCE_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/src
-                        BINARY_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/build
-                        TMP_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/tmp
-                        STAMP_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/stamp
-                        INSTALL_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/install
+                        PREFIX ${${LIBRARY_UPPERNAME}_PREFIX}
+                        DOWNLOAD_DIR ${${LIBRARY_UPPERNAME}_DOWNLOAD_DIR}
+                        SOURCE_DIR ${${LIBRARY_UPPERNAME}_SOURCE_DIR}
+                        BINARY_DIR ${${LIBRARY_UPPERNAME}_BINARY_DIR}
+                        TMP_DIR ${${LIBRARY_UPPERNAME}_TMP_DIR}
+                        STAMP_DIR ${${LIBRARY_UPPERNAME}_STAMP_DIR}
+                        INSTALL_DIR ${${LIBRARY_UPPERNAME}_INSTALL_DIR}
                         URL ${${LIBRARY_UPPERNAME}_URL}
                         URL_MD5 ${${LIBRARY_UPPERNAME}_URL_MD5}
                         CMAKE_ARGS ${${LIBRARY_UPPERNAME}_ARGS}
@@ -23,7 +31,9 @@ function(DownloadThenInstall LIBRARY_NAMES)
                         LOG_TEST 1
                         LOG_INSTALL 1
                        )
-  endforeach(LIBRARY_NAME)
+    set(${LIBRARY_UPPERNAME}_INSTALL_DIR ${${LIBRARY_UPPERNAME}_PREFIX}/install PARENT_SCOPE)
+
+endforeach(LIBRARY_NAME)
 
 
 endfunction(DownloadThenInstall)
