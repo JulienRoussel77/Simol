234c49d60b5a37de37ef42c9ba87ce68b36dbdfb
Cedric Doucet <cedric.doucet@inria.fr>
2015-05-21 10:39:02 +0200

    Add FindBoost and FindYamlcpp

--- cmake/modules/FindEigen.cmake
+++ cmake/modules/FindEigen.cmake
@@ -1,12 +1,9 @@
-# Define the following variables:
-# - EIGEN_INSTALL_DIR (if not user-defined)
-
 if(EXISTS ${EIGEN_INSTALL_DIR})
   message(STATUS "Eigen is declared to be installed in ${EIGEN_INSTALL_DIR}")
 else()
   if(ENABLE_DOWNLOAD)
-    list(APPEND LIBRARIES_TO_DOWNLOAD EIGEN)
-#    setDirectoryTree(Eigen)
+    set(LIBRARIES_TO_DOWNLOAD ${LIBRARIES_TO_DOWNLOAD} EIGEN)
+#    list(APPEND LIBRARIES_TO_DOWNLOAD EIGEN)
     set(EIGEN_URL http://bitbucket.org/eigen/eigen/get/3.2.4.tar.gz)
     set(EIGEN_URL_MD5 4d0d77e06fef87b4fcd2c9b72cc8dc55)
     set(EIGEN_ARGS -D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>)
---
+++ cmake/modules/FindYamlcpp.cmake
@@ -0,0 +1,17 @@
+if(EXISTS ${YAMLCPP_INSTALL_DIR})
+  message(STATUS "Yaml-cpp is declared to be installed in ${YAMLCPP_INSTALL_DIR}")
+else()
+  if(ENABLE_DOWNLOAD)
+    set(LIBRARIES_TO_DOWNLOAD ${LIBRARIES_TO_DOWNLOAD} YAMLCPP)
+#    list(APPEND LIBRARIES_TO_DOWNLOAD YAMLCPP)
+    set(YAMLCPP_URL https://yaml-cpp.googlecode.com/files/yaml-cpp-0.5.1.tar.gz)
+    set(YAMLCPP_URL_MD5 0fa47a5ed8fedefab766592785c85ee7)
+    set(YAMLCPP_ARGS -D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>)
+  else(ENABLE_DOWNLOAD)
+    message(FATAL_ERROR "Yaml-cpp is not found. Please provide a value to YAMLCPP_DIR or set ENABLE_DOWNLOAD to True")
+  endif()
+endif()
+
+
+
+
