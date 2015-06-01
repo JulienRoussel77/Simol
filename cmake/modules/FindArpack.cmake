6182c579132abaebe11092337ad0efffef404310
Cedric Doucet <cedric.doucet@inria.fr>
2015-06-01 16:04:55 +0200

    Automation of Arpack's installation

---
+++ cmake/modules/FindArpack.cmake
@@ -0,0 +1,17 @@
+if(EXISTS ${ARPACK_INSTALL_DIR})
+  message(STATUS "Arpack is declared to be installed in ${ARPACK_INSTALL_DIR}")
+else()
+  if(ENABLE_DOWNLOAD)
+    set(LIBRARIES_TO_DOWNLOAD ${LIBRARIES_TO_DOWNLOAD} ARPACK)
+    set(ARPACK_URL http://www.caam.rice.edu/software/ARPACK/SRC/arpack96.tar.gz)
+    set(ARPACK_PATCH_COMMAND cd <SOURCE_DIR> 
+                          && wget http://www.caam.rice.edu/software/ARPACK/SRC/patch.tar.gz
+                          && tar xvzf patch.tar.gz
+                          && cp ${CMAKE_SOURCE_DIR}/cmake/arpack/CMakeLists.txt <SOURCE_DIR>)
+    set(ARPACK_ARGS -D CMAKE_INSTALL_PREFIX=<INSTALL_DIR>)
+    set(ARPACK_INSTALL_COMMAND cp <BINARY_DIR>/libarpack.a <INSTALL_DIR>)
+  else(ENABLE_DOWNLOAD)
+    message(FATAL_ERROR "Arpack not found. Please provide a value to ARPACK_INSTALL_DIR or set ENABLE_DOWNLOAD to True")
+  endif()
+endif()
+
