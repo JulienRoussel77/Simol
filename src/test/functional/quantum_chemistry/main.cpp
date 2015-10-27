85fe1de8d204bf2dba2e5e3ced935651b43f606b
Cedric Doucet <cedric.doucet@inria.fr>
2015-10-27 16:27:24 +0100

    Modify main function to read LiH data (chemistry)

--- src/test/functional/quantum_chemistry/main.cpp
+++ src/test/functional/quantum_chemistry/main.cpp
@@ -17,11 +17,21 @@
 int main()
 {
 
-  std::string const dataPath = "/home/cdoucet/Documents/soutien/matherials/codes/simol/src/test/data/quantum_chemistry/";
+  std::string const dataPath = "/home/cdoucet/Documents/soutien/matherials/simol/src/test/data/LiH/";
 
-  simol::SparseMatrix<double> kineticMatrix(simol::MatrixMarketFile(dataPath+"kinetic_matrix.mtx"));
+  simol::MatrixMarketFile kinetic_file(dataPath + "kinetic_matrix.mtx");
+  simol::MatrixMarketFile overlap_file(dataPath + "overlap_matrix.mtx");
+  simol::MatrixMarketFile potential_file(dataPath + "potential_matrix.mtx");
+  simol::MatrixMarketFile twoelectron_file(dataPath + "twoelectron_matrix.mtx");
+  
+  simol::DenseMatrix<double> kinetic_matrix(kinetic_file);
+  simol::DenseMatrix<double> overlap_matrix(overlap_file);
+  simol::DenseMatrix<double> potential_matrix(potential_file);
+  simol::SparseMatrix<double> twoelectron_matrix(twoelectron_file);
+  
+  /*simol::SparseMatrix<double> kineticMatrix(simol::MatrixMarketFile(dataPath+"kinetic_matrix.mtx"));
   simol::SparseMatrix<double> overlapMatrix(simol::MatrixMarketFile(dataPath+"overlap_matrix.mtx"));
-  simol::SparseMatrix<double> potentialMatrix(simol::MatrixMarketFile(dataPath+"potential_matrix.mtx"));
+  simol::SparseMatrix<double> potentialMatrix(simol::MatrixMarketFile(dataPath+"potential_matrix.mtx"));*/
   return EXIT_SUCCESS;
 }
 
