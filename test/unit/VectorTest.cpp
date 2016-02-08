d23aceb861c2ad6237633f9197f363bd25a5f79b
Cedric Doucet <cedric.doucet@inria.fr>
2016-02-08 11:57:00 +0100

    Very first example of test unit with gtest

--- test/unit/VectorTest.cpp
+++ test/unit/VectorTest.cpp
@@ -1,34 +1,16 @@
 
-#include "VectorTest.hpp"
-
-#include <cppunit/extensions/HelperMacros.h>
+#include "gtest/gtest.h"
+#include "Vector.hpp"
 
 namespace simol
 {
 
-  CPPUNIT_TEST_SUITE_REGISTRATION( VectorTest );
-
-  void VectorTest::test_size()
+  TEST(VectorTest, SizeIsCorrect)
   {
     size_t expected_size = 10;
     Vector<double> vector(expected_size);
     size_t actual_size = vector.size();
-    CPPUNIT_ASSERT_EQUAL(expected_size, actual_size);
-  }
-
-  void VectorTest::test_sum()
-  {
-    Vector<double> left(3);
-    Vector<double> right(3);
-    Vector<double> sum(3);
-
-    for (size_t i=0;i<3;++i)
-    {
-      left(i) = 1;
-      right(i) = 2;
-      sum(i) = 3;
-    }
-    //CPPUNIT_ASSERT_EQUAL(sum, left+right);
+    EXPECT_EQ(expected_size, actual_size);
   }
 
 }
