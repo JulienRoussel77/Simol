4593190f904fd4814ae51638415701db80446d38
Cedric Doucet <cedric.doucet@inria.fr>
2016-02-09 17:01:01 +0100

    Clean DenseMatrix.hpp up

--- src/core/linalg/DenseMatrix.hpp
+++ src/core/linalg/DenseMatrix.hpp
@@ -11,8 +11,6 @@
 
 namespace simol
 {
-  //template<class ScalarType>
-  //using DenseMatrix = Eigen::Matrix<ScalarType, Eigen::Dynamic, Eigen::Dynamic>;
 
   template<class ScalarType, template<class> class WrappedLibrary = eigen>
   class DenseMatrix;
@@ -21,65 +19,93 @@ namespace simol
   class DenseMatrix<ScalarType,eigen>
   {
       public:
-          DenseMatrix(std::size_t numberOfRows,
-                      std::size_t numberOfColumns)
-          : wrapped_(numberOfRows, numberOfColumns)
-          {}
 
           DenseMatrix(DenseMatrix const & matrix) = default;
 
-          DenseMatrix(typename eigen<ScalarType>::DenseMatrixType const & wrappedMatrix)
-          : wrapped_(wrappedMatrix)
-          {}
+          DenseMatrix(std::size_t numberOfRows,
+                      std::size_t numberOfColumns);
 
-          Vector<ScalarType> column(size_t const index)
-          {
-              Vector<ScalarType> columnVector(wrapped_.rows());
-              columnVector.wrapped_.col(index);
-              return columnVector;
-          }
+          DenseMatrix(typename eigen<ScalarType>::DenseMatrixType const & wrappedMatrix);
 
+          Vector<ScalarType>
+          column(size_t const index);
 
-          DenseMatrix(MatrixMarketFile const & file)
-          : wrapped_(eigen<double>::DenseMatrixType::Zero(file.numberOfRows(), file.numberOfColumns()))
-          {
-            std::vector< Eigen::Triplet<ScalarType, std::size_t> > nonzeros(file.numberOfNonzeros());
-            for(size_t nonzeroIndex = 0; nonzeroIndex < file.numberOfNonzeros(); ++nonzeroIndex)
-            {
-              int rowIndex;
-              int columnIndex;
-              ScalarType nonzero;
-              fscanf(file.content(), "%d %d %lg\n", &rowIndex, &columnIndex, &nonzero);
-              wrapped_(rowIndex, columnIndex) = nonzero;
-            }
-          }
+          DenseMatrix(MatrixMarketFile const & file);
 
-          /*typename eigen<ScalarType>::AdjointReturnType
-          adjoint() const
-          { return wrapped_.adjoint(); }*/
+          ScalarType const &
+          operator()(size_t const rowIndex,
+                     size_t const columnIndex) const;
 
-          ScalarType const & operator()(size_t const rowIndex,
-                                        size_t const columnIndex) const
-          { return wrapped_(rowIndex, columnIndex); }
+          ScalarType &
+          operator()(size_t const rowIndex,
+                     size_t const columnIndex);
 
-          ScalarType & operator()(size_t const rowIndex,
-                                  size_t const columnIndex)
-          { return wrapped_(rowIndex, columnIndex); }
+          size_t
+          numberOfRows() const;
 
-          size_t numberOfRows() const
-          { return wrapped_.rows(); }
+          size_t
+          numberOfColumns() const;
 
-          size_t numberOfColumns() const
-          { return wrapped_.cols(); }
-
-          double rcond() const;
+          double
+          rcond() const;
 
       public:
           typename eigen<ScalarType>::DenseMatrixType wrapped_;
   };
 
   template<class ScalarType>
-  double
+  DenseMatrix<ScalarType, eigen>::DenseMatrix(std::size_t numberOfRows,
+              std::size_t numberOfColumns)
+  : wrapped_(numberOfRows, numberOfColumns)
+  {}
+
+  template<class ScalarType>
+  DenseMatrix<ScalarType, eigen>::DenseMatrix(typename eigen<ScalarType>::DenseMatrixType const & wrappedMatrix)
+  : wrapped_(wrappedMatrix)
+  {}
+
+  template<class ScalarType> Vector<ScalarType>
+  DenseMatrix<ScalarType, eigen>::column(size_t const index)
+  {
+      Vector<ScalarType> columnVector(wrapped_.rows());
+      columnVector.wrapped_.col(index);
+      return columnVector;
+  }
+
+  template<class ScalarType>
+  DenseMatrix<ScalarType, eigen>::DenseMatrix(MatrixMarketFile const & file)
+  : wrapped_(eigen<double>::DenseMatrixType::Zero(file.numberOfRows(), file.numberOfColumns()))
+  {
+    std::vector< Eigen::Triplet<ScalarType, std::size_t> > nonzeros(file.numberOfNonzeros());
+    for(size_t nonzeroIndex = 0; nonzeroIndex < file.numberOfNonzeros(); ++nonzeroIndex)
+    {
+      int rowIndex;
+      int columnIndex;
+      ScalarType nonzero;
+      fscanf(file.content(), "%d %d %lg\n", &rowIndex, &columnIndex, &nonzero);
+      wrapped_(rowIndex, columnIndex) = nonzero;
+    }
+  }
+
+  template<class ScalarType> ScalarType const &
+  DenseMatrix<ScalarType, eigen>::operator()(size_t const rowIndex,
+                                             size_t const columnIndex) const
+  { return wrapped_(rowIndex, columnIndex); }
+
+  template<class ScalarType> ScalarType &
+  DenseMatrix<ScalarType, eigen>::operator()(size_t const rowIndex,
+                                             size_t const columnIndex)
+  { return wrapped_(rowIndex, columnIndex); }
+
+  template<class ScalarType> size_t
+  DenseMatrix<ScalarType, eigen>::numberOfRows() const
+  { return wrapped_.rows(); }
+
+  template<class ScalarType> size_t
+  DenseMatrix<ScalarType, eigen>::numberOfColumns() const
+  { return wrapped_.cols(); }
+
+  template<class ScalarType> double
   DenseMatrix<ScalarType, eigen>::rcond() const
   {
     Eigen::JacobiSVD<Eigen::MatrixXd> svd(wrapped_);
