774f8caded7a53e3323b4e4ac0abc32941fe68e8
Cedric Doucet <cedric.doucet@inria.fr>
2016-02-17 17:50:03 +0100

    Load data for testing of hartree-fock on LiH

--- test/functional/quantchem/HartreeFockTest.cpp
+++ test/functional/quantchem/HartreeFockTest.cpp
@@ -6,10 +6,17 @@ namespace simol
 
   TEST(HartreeFockTest, LiH)
   {
-    /*std::string const dataPath = "LiH/";
+    std::string const dataPath = "/local/cdoucet/simol/test/functional/quantchem/LiH/";
     simol::MatrixMarketFile kinetic_file(dataPath + "kinetic_matrix.mtx");
     simol::MatrixMarketFile overlap_file(dataPath + "overlap_matrix.mtx");
-    simol::MatrixMarketFile potential_file(dataPath + "potential_matrix.mtx");*/
+    simol::MatrixMarketFile potential_file(dataPath + "potential_matrix.mtx");
+
+    simol::DenseMatrix<double> kinetic_matrix(kinetic_file);
+    simol::DenseMatrix<double> overlap_matrix(overlap_file);
+    simol::DenseMatrix<double> potential_matrix(potential_file);
+    //simol::SparseTensor<double> twoelectron_matrix(dataPath + "twoelectron_matrix.txt", M_disc);*/
+
+
     EXPECT_EQ(1,1);
 
   }
