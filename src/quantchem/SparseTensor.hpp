4e2096668f8ffba16bd8d635b60ca6f89cc34c34
Cedric Doucet <cedric.doucet@inria.fr>
2016-03-25 13:27:33 +0100

    Fix linking errors

--- src/quantchem/SparseTensor.hpp
+++ src/quantchem/SparseTensor.hpp
@@ -13,6 +13,15 @@
 
 namespace simol
 {
+    std::size_t
+    getInd(std::size_t const M_disc,
+           std::size_t const rowIndex,
+           std::size_t const columnIndex)
+    {
+        assert(rowIndex < M_disc && columnIndex < M_disc);
+        return rowIndex * M_disc + columnIndex;
+    }
+
     template<typename ScalarType = double>
     class SparseTensor
     {
