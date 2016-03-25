56854b5506eed67ff7710ca9733fe1bdb98dff4a
Gabriel Stoltz <stoltz@pl-59090.rocq.inria.fr>
2016-03-25 15:18:12 +0100

    heat capacity in input

--- src/statphys/input/input.hpp
+++ src/statphys/input/input.hpp
@@ -17,42 +17,42 @@ namespace simol{
 
   class Input{
     YAML::Node data;
-		string inputPath_;
-		ifstream inputFlux_;
+    string inputPath_;
+    ifstream inputFlux_;
     double positionMin_, positionMax_;
-		ifstream inputSettings_;
-		vector<Vector<double>> initialPositions_, initialMomenta_;
+    ifstream inputSettings_;
+    vector<Vector<double>> initialPositions_, initialMomenta_;
     std::shared_ptr<RNG> rng_;
   public:
     Input(CommandLine cmd);
-		const string& inputPath() const;
-		const std::ifstream& inputFlux() const;
-
-		string simuTypeName() const;
-		string parametersName() const;
+    const string& inputPath() const;
+    const std::ifstream& inputFlux() const;
+    
+    string simuTypeName() const;
+    string parametersName() const;
     string outputFolderName() const;
     const std::shared_ptr<RNG>& rng() const;
     std::shared_ptr<RNG>& rng();
-
-		// Geometry
+    
+    // Geometry
     int dimension() const;
     double length() const;
-
+    
     // Mesh/Time
     double timeStep() const;
-		size_t nbOfIterations() const;
-		size_t nbOfThermalIterations() const;
-		size_t nbOfBurningIterations() const;
-
-		// Physics/System
-		string systemName() const;
+    size_t nbOfIterations() const;
+    size_t nbOfThermalIterations() const;
+    size_t nbOfBurningIterations() const;
+    
+    // Physics/System
+    string systemName() const;
     size_t nbOfParticles() const;
     double mass() const;
-		bool doFileSettings() const;
-		string settingsPath() const;
-		Vector<double> initialPosition(int const& i=0) const;
+    bool doFileSettings() const;
+    string settingsPath() const;
+    Vector<double> initialPosition(int const& i=0) const;
     Vector<double> initialMomentum(int const& i=0) const;
-		// Physics/Potential
+    // Physics/Potential
     string potentialName() const;
     //Sinus
     double amplitude() const;
@@ -64,7 +64,7 @@ namespace simol{
     double potentialAlpha() const;
     double potentialBeta() const;
 
-		// Physics/Model
+    // Physics/Model
     string dynamicsName() const;
     double gamma() const;
     double temperature() const;
@@ -75,34 +75,34 @@ namespace simol{
     double betaRight() const;
     double externalForce() const;
     double tauBending() const;
-		double xi() const;
-		int seed() const;
-		double eta() const;
+    double xi() const;
+    int seed() const;
+    double eta() const;
+    double heatCapacity() const;  // used for DPDE dynamics
 
-
-
-		// Output
+    
+    // Output
     size_t decorrelationNbOfIterations() const;
     double decorrelationTime() const;
-		size_t outputPeriodNbOfIterations() const;
+    size_t outputPeriodNbOfIterations() const;
     double outputPeriodTime() const;
-		size_t outputProfilePeriodNbOfIterations() const;
+    size_t outputProfilePeriodNbOfIterations() const;
     double outputProfilePeriodTime() const;
-		int nbOfAutocoPts() const;
-		bool doFinalFlow() const;
-		bool doFinalVelocity() const;
-
-		// Controle Variate
+    int nbOfAutocoPts() const;
+    bool doFinalFlow() const;
+    bool doFinalVelocity() const;
+    
+    // Controle Variate
     string controlVariateName() const;
-		string controlVariateCoeffsPath() const;
-
-		//Galerkin
-		bool doGalerkinCV() const;
-		bool isGalerkin() const;
-		string galerkinElts() const;
-		size_t nbOfFourier() const;
-		size_t nbOfHermite() const;
-
+    string controlVariateCoeffsPath() const;
+    
+    //Galerkin
+    bool doGalerkinCV() const;
+    bool isGalerkin() const;
+    string galerkinElts() const;
+    size_t nbOfFourier() const;
+    size_t nbOfHermite() const;
+    
   };
 
 
