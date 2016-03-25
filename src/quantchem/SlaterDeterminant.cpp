b4f256f5327162931598ff44af54374a700699eb
Cedric Doucet <cedric.doucet@inria.fr>
2016-03-25 15:51:37 +0100

    Separate declaration from implementation in SlaterDeterminant

---
+++ src/quantchem/SlaterDeterminant.cpp
@@ -0,0 +1,31 @@
+#include "SlaterDeterminant.hpp"
+
+namespace simol
+{
+    SlaterDeterminant::SlaterDeterminant(DenseMatrix<double> const & matrix)
+    : matrix_(matrix)
+    {}
+
+    std::size_t SlaterDeterminant::number_of_electrons() const
+    { return matrix_.numberOfColumns(); }
+
+    DenseMatrix<double> const &
+    SlaterDeterminant::matrix() const
+    { return matrix_; }
+
+    DenseMatrix<double>
+    Smat(SlaterDeterminant const & Phi,
+         SlaterDeterminant const & Psi,
+         DenseMatrix<double> const & overlap) // mal nommé : utilisé avec H
+    {
+        DenseMatrix<double> U = Phi.matrix();
+        DenseMatrix<double> V = Psi.matrix();
+
+        // recupere la partie non stockee (symetrique)
+        DenseMatrix<double> temp(overlap.numberOfRows(), V.numberOfColumns());
+        temp = overlap * V;
+
+        return DenseMatrix<double>(U.adjoint() * temp);
+    }
+
+}
