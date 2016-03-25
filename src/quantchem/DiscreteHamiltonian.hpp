07a987c720940fe874a819f03c04f911f9ec54aa
Cedric Doucet <cedric.doucet@inria.fr>
2016-03-25 15:35:07 +0100

    Move SparseTensor from quantchem to linalg

--- src/quantchem/DiscreteHamiltonian.hpp
+++ src/quantchem/DiscreteHamiltonian.hpp
@@ -1,37 +1,24 @@
 #ifndef SIMOL_DISCRETEHAMILTONIAN_HPP
 #define	SIMOL_DISCRETEHAMILTONIAN_HPP
 
+#include "core/linalg/SparseTensor.hpp"
+
+#include <string>
+
 namespace simol
 {
+
     class DiscreteHamiltonian
     {
         public:
-           DiscreteHamiltonian(std::string pathToData, std::size_t basisDimension)
-           : kinetic_(pathToData + "kinetic_matrix.txt", basisDimension),
-             overlap_(pathToData + "overlap_matrix.txt", basisDimension),
-             potential_(pathToData + "potential_matrix.txt", basisDimension),
-             two_electrons_(pathToData + "twoelectron_matrix.txt", basisDimension)
-           {}
+           DiscreteHamiltonian(std::string pathToData, std::size_t basisDimension);
 
-           SparseMatrix<double> const &
-           kinetic() const
-           { return kinetic_; }
+           SparseMatrix<double> const & kinetic() const;
+           SparseMatrix<double> const & overlap() const;
+           SparseMatrix<double> const & potential() const;
+           SparseTensor<double> const & two_electrons() const;
 
-           SparseMatrix<double> const &
-           overlap() const
-           { return overlap_; }
-
-           SparseMatrix<double> const &
-           potential() const
-           { return potential_; }
-
-           SparseTensor<double> const &
-           two_electrons() const
-           { return two_electrons_; }
-
-            std::size_t
-            basisDimension() const
-            { return kinetic_.numberOfRows(); }
+           std::size_t basisDimension() const;
 
         private:
             SparseMatrix<double> kinetic_;
