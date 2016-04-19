86192b051a1a162e679a2ef190e027f09b7c2fe7
Cedric Doucet <cedric.doucet@inria.fr>
2016-04-19 18:55:53 +0200

    Pre-commit hook with astyle

--- git/pre-commit.sh
+++ git/pre-commit.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
- 
+
+# astyle options
 OPTIONS="--style=allman 
          --convert-tabs
          --indent=spaces=2 
          --indent-namespaces 
-         --indent-classes --indent-col1-comments --indent-switches --indent-labels --keep-one-line-blocks --pad-oper --suffix=none --formatted"
-RETURN=0
+         --indent-classes 
+         --indent-col1-comments 
+         --indent-switches 
+         --indent-labels 
+         --keep-one-line-blocks 
+         --pad-oper 
+         --suffix=none 
+         --formatted"
+
 ASTYLE=$(which astyle)
 
 if [ $? -ne 0 ]; then
@@ -19,4 +27,4 @@ exec 1>&2
 # Run astyle on all .cpp/.hpp files
 exec git diff --cached --name-only | grep -E "\.(cpp|hpp)" | xargs astyle $OPTIONS
 
-exit $RETURN
+exit 0
