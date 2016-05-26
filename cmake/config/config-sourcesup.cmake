14d7f3c6a40ef13866e42fc7b31c97019c3557a7
Cedric Doucet <cedric.doucet@inria.fr>
2016-05-26 10:56:42 +0200

    Configuration file for continuous integration on sourcesup

---
+++ cmake/config/config-sourcesup.cmake
@@ -0,0 +1,3 @@
+set(ENABLE_DOWNLOAD True CACHE BOOL "Automatic installation of thrid-party libraries")
+#set(SIMOL_GCC_ROOT "/usr" CACHE PATH "Root of GCC")
+set(EXTERNAL_DIR "/var/lib/jenkins/jobs/simol-diurne/workspace/download" PATH "")
