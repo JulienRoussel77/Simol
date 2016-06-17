7b9b5eb9862e5a90e1e11a3e7cd3e4d0093f9bfb
Cedric Doucet <cedric.doucet@inria.fr>
2016-06-17 11:15:19 +0200

    Fix typo

--- cmake/config/config.cmake
+++ cmake/config/config.cmake
@@ -1,3 +1,22 @@
-set(ENABLE_DOWNLOAD True CACHE BOOL "Automatic installation of thrid-party libraries")
-set(SIMOL_GCC_ROOT "/usr" CACHE PATH "Root of GCC")
-set(EXTERNAL_DIR "home/jurousse/Logiciels" PATH "")
+######################
+# Compilation settings
+######################
+
+SET(SIMOL_COMPILERS_ROOT "/local/cdoucet/gcc/4.9.2" CACHE PATH "Compilers root") # OPTIONAL -- alias for convenience
+
+SET(CMAKE_C_COMPILER "${SIMOL_COMPILERS_ROOT}/gcc" CACHE PATH "C compiler path") # REQUIRED
+SET(CMAKE_CXX_COMPILER "${SIMOL_COMPILERS_ROOT}/g++" CACHE PATH "C++ compiler path") # REQUIRED
+SET(CMAKE_Fortran_COMPILER "${SIMOL_COMPILERS_ROOT}/gfortran" CACHE PATH "Fortran compiler path") #REQUIRED
+
+#####################
+# 3rd party libraries
+#####################
+
+SET(SIMOL_AUTO_INSTALL True CACHE BOOL "Automatic downloading and installation of 3rd party libraries") # REQUIRED
+SET(SIMOL_EXTERNAL_DIR "/local/cdoucet/external" PATH "External libraries path")
+
+##############
+# Installation
+##############
+
+SET(SIMOL_INSTALL_PREFIX "/local/cdoucet/my_simol" PATH "Installation path")
