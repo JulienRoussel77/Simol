54aa55577e24a3f03733b3d8da852fd0f4e1ad53
Cedric Doucet <cedric.doucet@inria.fr>
2016-06-20 16:57:36 +0200

    Generic configuration file

--- cmake/config/config.cmake
+++ cmake/config/config.cmake
@@ -2,7 +2,7 @@
 # Compilation settings
 ######################
 
-SET(SIMOL_COMPILERS_ROOT "/local/cdoucet/gcc/4.9.2" CACHE PATH "Compilers root") # OPTIONAL -- alias for convenience
+SET(SIMOL_COMPILERS_ROOT "path where looking for compilers" CACHE PATH "Compilers root") # OPTIONAL -- alias for convenience
 
 SET(CMAKE_C_COMPILER "${SIMOL_COMPILERS_ROOT}/gcc" CACHE PATH "C compiler path") # REQUIRED
 SET(CMAKE_CXX_COMPILER "${SIMOL_COMPILERS_ROOT}/g++" CACHE PATH "C++ compiler path") # REQUIRED
@@ -13,10 +13,10 @@ SET(CMAKE_Fortran_COMPILER "${SIMOL_COMPILERS_ROOT}/gfortran" CACHE PATH "Fortra
 #####################
 
 SET(SIMOL_AUTO_INSTALL True CACHE BOOL "Automatic downloading and installation of 3rd party libraries") # REQUIRED
-SET(SIMOL_EXTERNAL_DIR "/local/cdoucet/external" PATH "External libraries path")
+# SET(SIMOL_EXTERNAL_DIR "path where installing or looking for external libraries" PATH "External libraries path")
 
 ##############
 # Installation
 ##############
 
-SET(SIMOL_INSTALL_PREFIX "/local/cdoucet/my_simol" PATH "Installation path")
+SET(SIMOL_INSTALL_PREFIX "path where you want to install simol" PATH "Installation path")
