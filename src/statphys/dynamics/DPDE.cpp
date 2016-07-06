6f1e0ce375620be9ba71e43b92e350a641e1b12a
Gabriel Stoltz <stoltz@pl-59090.paris.inria.fr>
2016-07-06 14:45:54 +0200

    new scheme for metropolization

--- src/statphys/dynamics/DPDE.cpp
+++ src/statphys/dynamics/DPDE.cpp
@@ -72,27 +72,42 @@ namespace simol
     double E0 = old_kin_energy + particle.internalEnergy(); 
     //-- propose a new move --
     Vector<double> G = rng_->gaussian();
-    Vector<double> b = effectiveDrift(particle);
-    particle.momentum() += b*timeStep_ + sigma()*sqrt(timeStep_)*G;
+    // EM scheme: not stable...
+    //Vector<double> b = effectiveDrift(particle);
+    //particle.momentum() += b*timeStep_ + sigma()*sqrt(timeStep_)*G;
+    // SSA like scheme: should be better
+    double gamma_n = gamma_DPDE(E0-old_kin_energy);
+    double alpha = exp(-gamma_n / particle.mass() * timeStep_);
+    double sigma_n = sqrt((1 - pow(alpha, 2)) * particle.mass() * temperature() * gamma() / gamma_n);
+    particle.momentum() = alpha * old_momentum + sigma_n * G;
     //-- compute the Metropolis rate --
     double new_kin_energy = particle.kineticEnergy();
     double rate = 0; 
     if (new_kin_energy < E0)
       {
      	rate = particle.mass()*heatCapacity()*(log(E0-new_kin_energy)-log(E0-old_kin_energy)) + 0.5*pow(G.norm(), 2); 
-	Vector<double> GG = old_momentum - particle.momentum() - effectiveDrift(particle)*timeStep_;
-	rate -= 0.5*pow(GG.norm(), 2)/(timeStep_*pow(sigma(),2));
-	rate = exp(rate);
+    	double gamma_reverse = gamma_DPDE(E0-new_kin_energy);
+    	double alpha_reverse = exp(-gamma_reverse / particle.mass() * timeStep_);
+    	double sigma2_reverse = (1 - pow(alpha_reverse,2)) * particle.mass() * temperature() * gamma() / gamma_reverse; 
+    	Vector<double> GG = old_momentum - alpha_reverse*particle.momentum();
+    	//to check... should be G
+    	//Vector<double> GGG = particle.momentum() - alpha*old_momentum;
+    	rate -= 0.5*pow(GG.norm(), 2)/sigma2_reverse;
+    	rate = exp(rate);
+    	//cout << rate << "; " << GG(0) << "; " << G << endl;
+	//	cout << new_kin_energy << " " << E0 << " and p = " << particle.momentum() << endl;
+    // 	cout << "sigma : " << sqrt(sigma2_reverse) << " vs " << sigma_n << "; alpha : " << alpha_reverse << " vs " << alpha << " but " << GG(0)/sqrt(sigma2_reverse) << " vs " << G << " which should be " << GGG(0)/sigma_n << " --> rate = " << rate << endl;
+	//if always want to accept: rate = 1;
       }
     //-- acceptance/rejection procedure --
     double U = rng_->scalarUniform();
     if (U > rate)
       {
-	//-- reject the move --
-	incrementRejection();
-	//cout << rate << "  " << rejectionCount() << endl;
-	//cout << rate << " ; " << E0 << " " << particle.momentum() << " , drift " << b(0) << ", previously " << old_momentum << ", internal = " << particle.internalEnergy() << endl;
-	particle.momentum() = old_momentum;
+    	//-- reject the move --
+    	incrementRejection();
+    	//cout << rate << "  " << rejectionCount() << endl;
+    	//cout << rate << " ; " << E0 << " " << particle.momentum() << " , GG " << GG(0) << ", previously " << old_momentum << ", internal = " << particle.internalEnergy() << endl;
+    	particle.momentum() = old_momentum;
       }
     else 
       {
--- src/statphys/simulation/Simulation_isolated.cpp
+++ src/statphys/simulation/Simulation_isolated.cpp
@@ -44,7 +44,8 @@ namespace simol
   {
     cout << " - Sampling the positions..." << endl;
     syst.getParticle(0).position(0) = 0;
-    syst.getParticle(0).internalEnergy() = 1;  // TO DO : sample according to equilibrium law?
+    syst.getParticle(0).momentum(0) = -1.41787;
+    syst.getParticle(0).internalEnergy() = 0.0696405;  // TO DO : sample according to equilibrium law?
     dyna.rejectionCount() = 0;
   }
 
