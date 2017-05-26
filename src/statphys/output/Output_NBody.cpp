e03d42659a9e6dde2c8d1c330c800ee16933a165
Julien Roussel <julien.roussel@enpc.fr>
2017-05-26 17:01:37 +0200

    output xmakemol improved

--- src/statphys/output/Output_NBody.cpp
+++ src/statphys/output/Output_NBody.cpp
@@ -4,19 +4,26 @@ namespace simol
 {
   ///
   ///-- display the current configuration in XMakemol format ; specific for NBody systems ---
-  void Output::displayXMakeMol(System const& syst, long int iOfStep, double domainSize)
+  void Output::displayXMakeMol(System const& syst, long int iOfStep, double /*domainSize*/)
   {
     outXMakeMol() << nbOfParticles_ << endl;
     outXMakeMol() << "Time = " << iOfStep * timeStep() << endl;
-    double coordinate = 0;
-    int Dim = dimension_;
+    //double coordinate = 0;
+    //int Dim = dimension_;
+    DVec pos = DVec::Zero(3);
     for (int iOfParticle = 0; iOfParticle < nbOfParticles_; iOfParticle++)
     {
       if (syst(iOfParticle).type() == 0)
         outXMakeMol() << " O  ";
+      else if (syst(iOfParticle).type() == -1)
+        outXMakeMol() << " He  ";
       else
-        outXMakeMol() << " C  ";
-      if (Dim == 3)
+        outXMakeMol() << " Ar  ";
+      
+      pos.head(dimension()) = syst.periodicImage(syst(iOfParticle).position());
+      outXMakeMol() << pos.adjoint() << endl;
+      
+      /*if (Dim == 3)
       {
         for (int dim = 0; dim < Dim; dim++)
         {
@@ -39,8 +46,8 @@ namespace simol
         outXMakeMol() << 0 << " ";
       }
       outXMakeMol() << endl;
+    }*/
+
     }
-
   }
-
 }
